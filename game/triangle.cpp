37ec7aaa4e42ab168ea41943d44207d80873e338
InsaneZeroGame <41873131+InsaneZeroGame@users.noreply.github.com>
2024-02-16 11:18:56 +0800

    Setup Window

--- game/triangle.cpp
+++ game/triangle.cpp
@@ -4,5 +4,35 @@
 int main() 
 {
 	std::cout << "Hello Re3D" << std::endl;
+	GLFWwindow* window;
+
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
+
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Render here */
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 	return 0;
 }
